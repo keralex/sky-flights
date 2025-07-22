03280288c858e6c8dd05c64cf3e3e3176f92b868
keralex <kerlis.aguado26@gmail.com>
2025-07-21 21:09:38 -0300

    feat: add flight fetching service with API integration

---
+++ src/services/flightsService.ts
@@ -0,0 +1,25 @@
+const BASE_URL =
+    "https://sky-scrapper.p.rapidapi.com/api/v1/flights/searchFlights";
+
+export const fetchFlights = async (params: Record<string, string>) => {
+    const url = new URL(BASE_URL);
+
+    Object.entries(params).forEach(([key, value]) => {
+        url.searchParams.append(key, value);
+    });
+
+    const response = await fetch(url.toString(), {
+        method: "GET",
+        headers: {
+            "x-rapidapi-key": import.meta.env.VITE_RAPIDAPI_KEY!,
+            "x-rapidapi-host": "sky-scrapper.p.rapidapi.com",
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error fetching flights: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+};
